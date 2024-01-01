1d7b50b041c11bc9f9b9b04e75b86b32a05ef901
Muhammed <kgokgun94@gmail.com>
2024-01-01 11:53:34 +0300

    CountryAPI interface created.

--- app/src/main/java/com/muhammedkursat/kotlincountries/model/Model.kt
+++ app/src/main/java/com/muhammedkursat/kotlincountries/model/Model.kt
@@ -1,12 +1,19 @@
 package com.muhammedkursat.kotlincountries.model
 
 import android.media.Image
+import com.google.gson.annotations.SerializedName
 
 data class Country(
+    @SerializedName("name")
     val countryName: String?,
+    @SerializedName("region")
     val countryRegion: String?,
+    @SerializedName("capital")
     val countryCapital: String?,
+    @SerializedName("flag")
     val countryFlag: String?,
+    @SerializedName("currency")
     val countryCurrency: String?,
+    @SerializedName("language")
     val countryLanguage: String?
 )
---
+++ app/src/main/java/com/muhammedkursat/kotlincountries/sevice/CountryAPIService.kt
@@ -0,0 +1,7 @@
+package com.muhammedkursat.kotlincountries.sevice
+
+class CountryAPIService {
+    //https://raw.githubusercontent.com/atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
+    //BASE>>> https://raw.githubusercontent.com/
+    // EXT>>> atilsamancioglu/IA19-DataSetCountries/master/countrydataset.json
+}
